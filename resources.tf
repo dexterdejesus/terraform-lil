02d58eb72e13756c9557fde934e882ef83c7cff1
dexterfahddejesus <dexter.dejesus@tooltwist.com>
2018-08-09 16:52:04 +0800

    update

--- resources.tf
+++ resources.tf
@@ -3,12 +3,37 @@ resource "aws_vpc" "environment-example" {
 	enable_dns_hostnames = true
 	enable_dns_support = true
 	tags {
-	  name = "terraform-aws-vpc-example"
+	  Name = "terraform-aws-vpc-example"
 	}
 }
 
-resource "aws_subnet" "" {
-	name = ""
-	cidr_block = ""
+resource "aws_subnet" "subnet1" {
+	cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block,3 ,1 )}"
 	vpc_id = "${aws_vpc.environment-example.id}"
+	availability_zone = "ap-northeast-1a"
+	tags {
+		Name = "tf-1a"
+	}
+}
+
+resource "aws_subnet" "subnet2" {
+	cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block,2 ,2 )}"
+	vpc_id = "${aws_vpc.environment-example.id}"
+	availability_zone = "ap-northeast-1c"
+	tags {
+		Name = "tf-1c"
+	}
+}
+
+resource "aws_security_group" "subnetsecurity" {
+	vpc_id = "${aws_vpc.environment-example.id}"
+
+	ingress {
+		cidr_blocks = [
+		 "${aws_vpc.environment-example.cidr_block}"
+		]
+		from_port = 80
+		protocol = "tcp"
+		to_port = 80
+	}
 }
